0a579d090be6b63e56197556a53ea08685ac8c2b
Erinad-2002 <Erinad.business@gmail.com>
2021-01-23 10:50:28 +0100

    add project base

---
+++ PIxel_Roguelike/Assets/CollisionDetect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
